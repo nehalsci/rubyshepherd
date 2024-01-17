8eb6f946abdddc9276b17b682ea6335276d9c0df
nehalsci <155372686+nehalsci@users.noreply.github.com>
2024-01-17 16:14:36 +0000

    feat(main): three ways to print string

--- main.rb
+++ main.rb
@@ -1,1 +1,20 @@
-puts 'Hello, World!'
+# Three ways to print string.
+
+# 1. put(s -> string) {data}
+puts 'Hello Dad!'
+
+# 2. put(s -> string) {data from variable}
+message = 'Hello Mom!'
+puts message
+
+message = 'Vary~'; # Reassign the variable message
+puts message
+
+# 3. Print through a method/function
+def say_hello(name)
+  puts "Hello #{name}!"
+end
+
+say_hello('World')
+# The advantage of using method is that you can reuse the code and pass different string.
+say_hello 'World' # Parenthesis is not mandatory.
