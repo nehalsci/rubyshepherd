84bed1debb0bab3f91f31d2127fc94cf65169cbd
nehalsci <155372686+nehalsci@users.noreply.github.com>
2024-01-17 18:05:41 +0000

    refc(powerNum): use method for product

--- creations/powerNum.rb
+++ creations/powerNum.rb
@@ -1,19 +1,19 @@
 # This programs takes a number and a second number which is the number of times to multiply the first number by itself.
 
+def pow(num, pow)
+  product = 1
+
+  pow.times do
+    product *= num;
+  end
+
+  return product;
+end
+
 puts 'Enter your number'
 num = gets.chomp; # This returns the number as a string.
-num = num.to_i
 
 puts 'Enter the power'
 power = gets.chomp
 
-product = nil
-power.to_i.times do
-  product = if product.nil?
-              num
-            else # This formatting is done by Replit. (I am not involved.)
-              product * num
-            end
-end
-
-puts "The product is #{product}"
+puts "The product is #{pow num.to_i, power.to_i}";
